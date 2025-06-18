8cf6a3eb277302394ab8276beb610141f9ba3ac6
Smartbot1x <smartbot616@gmail.com>
2025-06-19 00:14:46 +0200

    layout updated

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
 import Footer from "./components/Footer";
-import HeaderComponent from "./components/Header";
+import HeaderComponent from "./components/header";
 import "./App.css";
 /* import Spinner from "./Pages/Load"; */
 import Chatbot from "./components/Chatbot";
